4ccd25a533e9106b9d73e871e36eeec31893b101
matdubuisson <matteo.dubuisson@student.uclouvain.be>
2023-11-11 18:11:12 +0100

    test

--- Tetris.py
+++ Tetris.py
@@ -1,4 +1,5 @@
 import numpy as np
+import puzzles as pz
 
 class Tetris:
     def __init__(self, width, height, block_size):
@@ -10,8 +11,30 @@ class Tetris:
         self.current_piece = None
         self.next_piece = None
         self.score = 0
+        self.puzzle = pz.Puzzle()
+        
+    def place_dispo(self, dispo: np.ndarray) -> tuple:
+        width = len(dispo)
+        height = len(dispo[0])
+        
+        for x in (0, 12 - width):
+            for y in range(0, 20 - height):
+                pass
+                
 
+    def generate_max(self, piece: str, x: int, y: int) -> tuple:
+        dispositions = self.puzzle.dp.get(piece)
+        
+        # (score, dispo, (x, y))
+        result = None
+        
+        for dispo in dispositions:
+            tmp = self.place_dispo(dispo)
 
-
-
-
+            if result[0] < tmp[0]:
+                result = tmp
+                
+        return result
+    
+    def get_score(self, dispo: np.ndarray, x: int, y: int) -> int:
+        pass
---
+++ puzzles.py
@@ -0,0 +1,129 @@
+import numpy as np
+
+class Puzzle:
+    I = np.array((
+        (
+            (1, 1, 1, 1)
+        ,),
+        (
+            (1),
+            (1),
+            (1),
+            (1)
+        )
+    ), np.uint8)
+    
+    O = np.array((
+        ((
+            (1, 1),
+            (1, 1)
+        ,), np.array((0, 0), np.uint0))
+    ,), np.uint8)
+    
+    T = np.array((
+        ((
+            (0, 1, 0),
+            (1, 1, 1)
+        ), np.array((0, 0, 0), np.uint8)),
+        ((
+            (1, 0),
+            (1, 1),
+            (1, 0)
+        ), np.array((0, 1), np.uint8)),
+        ((
+            (1, 1, 1),
+            (0, 1, 0)
+        ), np.array((1, 0, 1), np.uint8)),
+        ((
+            (0, 1),
+            (1, 1),
+            (0, 1)
+        ), np.array((1, 0), np.uint8))
+    ))
+    
+    L = np.array((
+        (
+            (1, 1, 1),
+            (1, 0, 0)
+        ),
+        (
+            (1, 1),
+            (0, 1),
+            (0, 1)
+        ),
+        (
+            (0, 0, 1),
+            (1, 1, 1)
+        ),
+        (
+            (1, 0),
+            (1, 0),
+            (1, 1)
+        )
+    ), np.uint8)
+    
+    J = np.array((
+        (
+            (1, 1, 1),
+            (0, 0, 1)
+        ),
+        (
+            (0, 1),
+            (0, 1),
+            (1, 1)
+        ),
+        (
+            (1, 0, 0),
+            (1, 1, 1)
+        ),
+        (
+            (1, 1),
+            (1, 0),
+            (1, 0)
+        )
+    ), np.uint8)
+    
+    Z = np.array((
+        (
+            (1, 1, 0),
+            (0, 1, 1)
+        ),
+        (
+            (0, 1),
+            (1, 1),
+            (1, 0)
+        )
+    ), np.uint8)
+    
+    S = np.array((
+        (
+            (0, 1, 1),
+            (1, 1, 0)
+        ),
+        (
+            (1, 0),
+            (1, 1),
+            (0, 1)
+        )
+    ), np.uint8)
+    
+    dp = {
+        "I": I,
+        "O": O,
+        "T": T,
+        "L": L,
+        "J": J,
+        "Z": Z,
+        "S": S
+    }
+
+    all = []
+    for key in dp.keys():
+        for each in dp[key]:
+            all.append(each)
+    all = tuple(all)
+    
+    def play(game, piece):
+        # pre : game is the tetris board and piece is a new piece to place
+        # post : 
+        return
