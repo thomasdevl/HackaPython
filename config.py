58cf439c9453ad1772295418d1afb85309395e2a
MxDx <maxime.dxl2002@gmail.com>
2023-11-11 23:53:50 +0100

    gitignore

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 # Change according to your screen resolution 
 
 # Value of the new game button
-new_game_x, new_game_y = 723, 455
+new_game_x, new_game_y = 734, 340
+
+# Value of ok button
+ok_x, ok_y = 1123, 223
 
 # Value of the first piece spawn point
-first_piece_x, first_piece_y = 836, 433
+first_piece_x, first_piece_y = 1107, 280
 
 # Value of the next piece spawn point
-next_piece_x, next_piece_y = 696, 576
+next_piece_x, next_piece_y = 748, 642
